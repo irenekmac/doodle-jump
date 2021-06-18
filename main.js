3dc2718e5e0786011dc4311b8a8816e6b05a21a3
irene macapinlac <irenekaymac@gmail.com>
2021-06-18 23:55:22 +1000

    create platform

--- main.js
+++ main.js
@@ -14,9 +14,26 @@ document.addEventListener('DOMContentLoaded', () => {
     doodler.style.bottom = doodlerBottomSpace + 'px'
   }
 
-  function createPlatforms() {
-    for (let i =0; i < platformCount; i++){
+  class Platform {
+    constructor(newPlatBottom) {
+      this.bottom = newPlatBottom
+      this.left = Math.random() * 315
+      this.visual = document.createElement('div')
 
+      const visual = this.visual
+      visual.classList.add('platform')
+      visual.style.left = this.left + 'px'
+      visual.style.bottom = this.bottom + 'px'
+      grid.appendChild(visual)
+    }
+
+  }
+
+  function createPlatforms() {
+    for (let i =0; i < platformCount; i++) {
+      let platGap = 600 / platformCount
+      let newPlatBottom = 100 + 1 * platGap
+      let newPlatform = new Platform()
     }
   }
 
